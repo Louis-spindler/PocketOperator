3f800699403842291b238c72ef103aba04a5a652
Louis-spindler <104321580+Louis-spindler@users.noreply.github.com>
2024-01-24 18:04:03 +0000

    added TrackButton.py to file to oranize the asigning of the track variables

---
+++ TrackButtons.py
@@ -0,0 +1,8 @@
+class TrackButton():
+  def __init__(self):
+    status = False
+
+
+a1,a2,a3,a4,a5,a6,a7,a8 = TrackButton(),TrackButton(),TrackButton(),TrackButton(),TrackButton(),TrackButton(),TrackButton(),TrackButton()
+b1,b2,b3,b4,b5,b6,b7,b8 = TrackButton(),TrackButton(),TrackButton(),TrackButton(),TrackButton(),TrackButton(),TrackButton(),TrackButton()
+c1,c2,c3,c4,c5,c6,c7,c8 = TrackButton(),TrackButton(),TrackButton(),TrackButton(),TrackButton(),TrackButton(),TrackButton(),TrackButton()
